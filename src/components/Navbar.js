c62044ca0dbcd6aff9264cdf4321fdef4534be29
donat3 <118883706+donat3@users.noreply.github.com>
2023-04-24 14:57:16 +0200

    ndryshim vogel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="NavbarItems">
-                <h1>Ruby</h1>
+                <h1>Ruby <i className="fab fa-react"></i></h1>
             </nav>
         )
     }
