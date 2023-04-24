a61de967648e44ccae9e883ed6ab7a2d8c74fdc1
donat3 <118883706+donat3@users.noreply.github.com>
2023-04-24 14:53:04 +0200

    shtimi i navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1>Ruby</h1>
+            </nav>
+        )
+    }
+}
