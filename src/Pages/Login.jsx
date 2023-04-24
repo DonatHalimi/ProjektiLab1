d18b34fbaffcd2285b7f617facf78f40bdeba0fb
MalMikullovci <malmikullovci1@gmail.com>
2023-04-24 17:06:36 +0200

    Shtimi i fushave per login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,18 @@
+import React,{useState} from "react";
+
+
 export const Login = () => {
   return (
-    <>
-      Login
-    </>
+   <form>
+    <label for="email">E-mail</label>
+    <input type="email" placeholder="Shtypni e-mail" id="email" name="email"></input>
+    <label for="password">Password</label>
+    <input type="password" placeholder="Shtypni password-in" id="password" name="password"></input>
+    <button>Log in </button>
+
+
+
+
+   </form>
   );
 };
