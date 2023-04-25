391ca45fe68b2c0dcb393dce086f5f0b1bf21dff
donat3 <118883706+donat3@users.noreply.github.com>
2023-04-25 22:34:50 +0200

    shtimi i buttonit to-top ne Home.js pastaj implementimi ne App.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
+import React, { useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
 import "./HomeStyle.css";
 
 function Home() {
-    return(
-    <>
-    <div className="home"></div>
-    </>
+    const [showButton, setShowButton] = useState(false);
+
+    const handleScroll = () => {
+        if (window.pageYOffset > 300) {
+            setShowButton(true);
+        } else {
+            setShowButton(false);
+        }
+    };
+
+    const handleClick = () => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return (
+        <>
+            <div className="home"></div>
+            {showButton && (
+                <button className="to-top-button" onClick={handleClick}><FaArrowUp /></button>
+            )}
+        </>
     );
 }
 
