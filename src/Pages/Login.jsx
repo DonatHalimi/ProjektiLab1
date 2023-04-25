abb053032d5c11d622937e69c13d05621074b05a
MalMikullovci <malmikullovci1@gmail.com>
2023-04-25 20:12:20 +0200

    Krijimi i struktures dhe dizajnit te log-init.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,32 @@
 import React,{useState} from "react";
+import "./Login.css";
 
 
 export const Login = () => {
-  return (
-   <form>
-    <label for="email">E-mail</label>
-    <input type="email" placeholder="Shtypni e-mail" id="email" name="email"></input>
-    <label for="password">Password</label>
-    <input type="password" placeholder="Shtypni password-in" id="password" name="password"></input>
-    <button>Log in </button>
+  const [email,setEmail]=useState('');
+  const [pass,setPass]=useState('');
 
-   </form>
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(email); 
+  }
+
+
+  return (
+    <div class="login-box">
+    <p>Log in</p>
+    <form>
+      <div class="user-box">
+      <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Shtypni e-mail" id="email" name="email"></input>
+        <label for='email'>Email</label>
+      </div>
+      <div class="user-box">
+      <input value={pass} onChange={(e)=>setPass(e.target.value)} type="password" placeholder="Shtypni password-in" id="password" name="password"></input>
+        <label for="password">Password</label>
+      </div>
+      <button class="btn" type="submit">Log in </button>
+    </form>
+    <p>Don't have an account? <a href="" class="a2">Sign up!</a></p>
+  </div>
   );
 };
