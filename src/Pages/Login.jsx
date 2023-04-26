fc333d904d445e45a011e5d1fc61a9c9223457a1
MalMikullovci <malmikullovci1@gmail.com>
2023-04-26 18:33:59 +0200

    Shtimi i validimit te login-page ne baze te nje regex-i

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,30 +1,69 @@
-import React,{useState} from "react";
+import {useState,useEffect} from "react";
 import "./LoginStyle.css";
 
 
 
 export const Login = () => {
-  const [email,setEmail]=useState('');
-  const [pass,setPass]=useState('');
+  const initialValues={email:"",password:""};
+  const [formValues,setFormValues]=useState(initialValues);
+  const [formErrors,setFormErrors]=useState({});
+  const [isSubmit,setIsSubmit]=useState(false);
 
+  
+  const handleChange =(e) => {
+    const{ name , value } =e.target;
+    setFormValues({...formValues,[name]:value});
+    
+  }
+  
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log(email); 
+    validate(formValues);
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
   }
+  useEffect(() =>{
+    if(Object.keys(formErrors).length=== 0 && isSubmit){
+      console.log(formValues);
+    }
+  },[formErrors]);
 
+  const validate = (values)=>{
+    const errors={}
+    const regexEmail=/^[a-z0-9._%+-]+@[a-z0-9.-]+\.(com|net)$/;
+    const regexPass=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+=-])[A-Za-z\d!@#$%^&*()_+=-]{8,}$/;
+
+    if(!values.email){
+      errors.email ="Email is required";
+    }else if(!regexEmail.test(values.email)){
+      errors.email="This is not a valid email"
+    }
+    if(!values.password){
+      errors.password ="Password is required";
+    }else if(!regexPass.test(values.password)){
+      errors.password="This is not a valid password"
+    }
+    return errors;
+
+  }
 
   return (
     <div class="login-box">
     <p>Log in</p>
-    <form>
+    <pre></pre>
+    <form onSubmit={handleSubmit}>
       <div class="user-box">
-      <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Type e-mail" id="email" name="email"></input>
+      <input value={formValues.email} onChange={handleChange} type="email" placeholder="Type e-mail" id="email" name="email"></input>
         <label htmlFor='email'>Email</label>
+        <p id="error">{formErrors.email}</p>
       </div>
+      
       <div class="user-box">
-      <input value={pass} onChange={(e)=>setPass(e.target.value)} type="password" placeholder="Type password" id="password" name="password"></input>
+      <input value={formValues.password} onChange={handleChange} type="password" placeholder="Type password" id="password" name="password"></input>
         <label htmlFor="password">Password</label>
+        <p id="error">{formErrors.password}</p>
       </div>
+      
       <button class="btn" type="submit" >Log in </button>
     </form>
     <p>Don't have an account? <a href="/register" class="a2">Sign up!</a></p>
