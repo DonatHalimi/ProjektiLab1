76f4855025067dce09b6e68def8fb00017adf8e3
MalMikullovci <malmikullovci1@gmail.com>
2023-04-28 12:41:48 +0200

    Krijimi dhe prova se a eshte lidhur mire databaza

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app=express();
+const mysql=require('mysql');
+
+
+const db = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'projektilab1'
+})
 
 
 
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+     
+    const sqlInsert = "INSERT INTO userat (Name, Surname, `E-mail`, Password, Role) VALUES ('Mal', 'Mikullovci', 'alibungu@gmail.com', 'HamdiHamdi123', 1)";
+    db.query(sqlInsert,(err,result)=>{
+        if (err) {
+            console.log(err); // log the error to the console
+            res.send("Error occurred: " + err.message); // return an error message to the client
+          } else {
+            console.log(result); // log the result to the console
+            res.send("Insert successful"); // return a success message to the client
+            res.send("hello wor");
+        }  
+    });
+    
 });
 
 app.listen(6001,()=>{
