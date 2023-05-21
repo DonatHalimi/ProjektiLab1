3d7fd073732a5d43a4b8d92eeb7ed9ea57c3e5a2
donat3 <118883706+donat3@users.noreply.github.com>
2023-05-21 17:52:39 +0200

    Fixed produktet me u shfaq prej databazes (Perveq fotove qe nuk po shtohen hala)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -15,41 +15,40 @@ function Home() {
 
     // Krijimi i nje funksioni per te kerkuar te dhenat nga API i produktit (paperfundume)
     useEffect(() => {
-        // Thirret funksioni kur komponenti ngarkohet per here te pare
-        fetch("http://localhost:6001/api/product")
-            .then((response) => {
-                // Nese kerkesa eshte e suksesshme kthen nje pergjigje ne formatin JSON (JavaScript Object Notation) - JSON ruan te dhena te perkohshme
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch("http://localhost:6001/api/product/get");
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
-                return response.json();
-            })
-            // Vendos vlerat e reja te produkteve ne variablen products
-            .then((data) => setProducts(data))
-            // Nese ndodh nje gabim paraqitet ne konsole
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            });
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+
+        fetchProducts();
     }, []);
 
     // Krijimi i nje vargu me te dhenat e produktit duke perdorur metoden map prej vargut product_card ne klasen ProductData
-    const listItems = product_card.map((item =>
-        <div className="card" key={item.id}>
-            <div className="card_img">
-                <img src={item.thumb}></img>
-            </div>
-            <div className="card_header">
-                <h2>{item.product_name}</h2>
-                <p>{item.description}</p>
-                <p className="price"> {item.price} <span>{item.currency}</span> </p>
-                <div className="butonat">
-                    <div className="buton" ><i class="fa-solid fa-heart"></i></div>
-                    <div className="btn" ><i class="fa-solid fa-shopping-cart"></i></div>
-                </div>
-            </div>
-        </div>
-    )
-    );
+    // const listItems = product_card.map((item =>
+    //     <div className="card" key={item.id}>
+    //         <div className="card_img">
+    //             <img src={item.thumb}></img>
+    //         </div>
+    //         <div className="card_header">
+    //             <h2>{item.product_name}</h2>
+    //             <p>{item.description}</p>
+    //             <p className="price"> {item.price} <span>{item.currency}</span> </p>
+    //             <div className="butonat">
+    //                 <div className="buton" ><i class="fa-solid fa-heart"></i></div>
+    //                 <div className="btn" ><i class="fa-solid fa-shopping-cart"></i></div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
+    // );
 
     // Renderimi i HTML per produkte ne main page
     return (
@@ -58,7 +57,7 @@ function Home() {
             <Navbar />
             <Slider />
 
-            <div>
+            {/* <div>
                 <h1>Featured Products</h1>
                 <div className="main-content" style={{
                     display: "flex",
@@ -69,28 +68,24 @@ function Home() {
                 }}>
                     {listItems}
                 </div>
-            </div>
+            </div> */}
 
-            {/* 
-            Paraqitja e produkteve ne main page me ane te databazes (paperfundume)
-
-            <div className="home-container">
+            <div>
                 <h1>Featured Products</h1>
-                <div className="product-list">
+                <div className="products-container">
                     {products.map((product) => (
                         <Product
                             key={product.idproduct}
                             idproduct={product.idproduct}
-                            emri={product.Emri}
-                            detajet={product.Detajet}
-                            kategoria={product.Kategoria}
-                            fotoSource={product.FotoSource}
+                            Emri={product.Emri}
+                            Cmimi={product.Cmimi}
+                            Detajet={product.Detajet}
+                            Kategoria={product.Kategoria}
+                            FotoSource={product.FotoSource}
                         />
                     ))}
                 </div>
-            </div> 
-            
-            */}
+            </div>
 
             {/* Thirrja e komponentit te Footer */}
             <Footer />
